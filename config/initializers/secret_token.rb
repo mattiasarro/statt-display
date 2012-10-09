cc39cd9eb81217f625c341a670c2dfb48d17cef1
Mattias Arro <mattias.arro@gmail.com>
2012-10-09 18:32:01 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Display::Application.config.secret_token = '980fa049d575ad15fb3b7047883dfdf6c6b6eab0f2174b91d5a980cc83a1fb9713b5a3b1c17a3ab7d0e3de079cb2f70b0eb82f2465cf5a0ca61e18a07e7820dc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Display::Application.config.session_store :cookie_store, key: '_display_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Display::Application.config.session_store :active_record_store
