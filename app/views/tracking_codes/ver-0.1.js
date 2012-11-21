42e8a60e6aa858223733513c380dd419a6a767b0
Mattias Arro <mattias.arro@gmail.com>
2012-11-21 18:52:12 +0000

    show correct tracking code

--- app/views/tracking_codes/ver-0.1.js
+++ app/views/tracking_codes/ver-0.1.js
@@ -1,3 +1,13 @@
 <script>
-  mkay
+  var _statt = _statt || [];
+  (function() {
+    var script = document.createElement('script');
+    script.type  = 'text/javascript';
+    script.async = true;
+    script.id    = 'statt-tracker';
+    script.setAttribute('data-site-id', '<%= @site.id %>');
+    script.src = '<%= Rails.configuration.collect_host %>/track.js';
+    var b = document.getElementsByTagName('script')[0];
+    b.parentNode.insertBefore(script, b);
+  })();
 </script>
