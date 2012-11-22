5c58cf8b04cc07dfc090146b1756f7ab3ae4d78e
Mattias Arro <mattias.arro@gmail.com>
2012-11-22 00:45:48 +0000

    allow setting cl_user_id via tracking code

--- app/views/tracking_codes/ver-0.1.js
+++ app/views/tracking_codes/ver-0.1.js
@@ -6,6 +6,7 @@
     script.async = true;
     script.id    = 'statt-tracker';
     script.setAttribute('data-site-id', '<%= @site.id %>');
+    // script.setAttribute('data-user_id', 'user-name');
     script.src = '<%= Rails.configuration.collect_host %>/track.js';
     var b = document.getElementsByTagName('script')[0];
     b.parentNode.insertBefore(script, b);
