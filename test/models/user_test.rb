47e148a8ee697f6bca7a501e875471857058b691
Mattias Arro <mattias.arro@gmail.com>
2012-12-25 03:40:50 +0000

    not necessary tests, for helping on the github issue

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,4 +10,43 @@ class UserTest < MiniTest::Rails::ActiveSupport::TestCase
     refute @user.admin?
   end
   
+  # for the Github issue to test HABTM
+  it "should have correct site_ids" do
+    @user = FactoryGirl.create(:user)
+    @site1 = Site.create
+    @site2 = Site.create
+    
+    @user.site_ids = [@site1.id, @site2.id]
+    @user.save
+    
+    @site1.reload.users.size.must_equal 1
+    @site2.reload.users.size.must_equal 1
+  end
+  
+  it "should have correct site_ids" do
+    @user = FactoryGirl.create(:user)
+    @user.sites.create
+    @user.sites.create
+    
+    @user.sites.size.must_equal 2
+    Site.first.users.size.must_equal 1
+    Site.last.users.size.must_equal 1
+  end
+  
+  it "should have correct site_ids" do
+    @user = FactoryGirl.create(:user)
+    
+    @site1 = Site.create
+    @site2 = Site.create
+    
+    @user.sites << @site1
+    @user.sites << @site2
+    
+    @user.save
+    
+    @user.sites.size.must_equal 2
+    @site1.reload.users.size.must_equal 1
+    @site2.reload.users.size.must_equal 1
+  end
+  
 end
