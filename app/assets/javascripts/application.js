fd2cebb1e3e8b0423efcd1bd75431053f865710f
Mattias Arro <mattias.arro@gmail.com>
2013-04-11 11:50:09 +0100

    JS load order, config.serve_static_assets, D3 precompile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,7 +4,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter/bootstrap
-//= require_tree .
+//= require d3
+//= require daterange_selector
+//= require graph_daterange
+//= require graph_rendering
+//= require loads
+//= require visitors
+//= require_self
 $(document).ready(function () {
   $("a[rel=popover]").popover();
   $(".tooltip").tooltip();
